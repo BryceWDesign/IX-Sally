72dcf8a8979ec4c3bdf765a0c147a10a528945e8
Bryce Lovell <40742705+BryceWDesign@users.noreply.github.com>
2026-06-28 11:50:50 -0700

    Add event-related classes to __init__.py
    Added RuntimeEvent, RuntimeEventType, and RuntimeTranscript to imports and __all__.

--- src/ix_sally/__init__.py
+++ src/ix_sally/__init__.py
@@ -12,6 +12,7 @@ from ix_sally.chamber import ObservationChamberConfig, StopCondition, StopReason
 from ix_sally.contracts import AutonomyContract, AutonomyMode
 from ix_sally.digest import DigestRecord
 from ix_sally.doctrine import DoctrineCatalog, DoctrineRule, DoctrineSeverity
+from ix_sally.events import RuntimeEvent, RuntimeEventType, RuntimeTranscript
 from ix_sally.foundation import CanonicalKey, FoundationError
 from ix_sally.jurisdiction import JurisdictionDecision, JurisdictionGate, JurisdictionStatus
 
@@ -32,6 +33,9 @@ __all__ = [
     "JurisdictionGate",
     "JurisdictionStatus",
     "ObservationChamberConfig",
+    "RuntimeEvent",
+    "RuntimeEventType",
+    "RuntimeTranscript",
     "StopCondition",
     "StopReason",
     "default_agent_role_registry",
